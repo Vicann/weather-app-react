c6f4f46aa1532ea54092abc3ec872af84f432db9
Victoria <39576407+Vicann@users.noreply.github.com>
2024-01-21 15:43:09 -0500

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
